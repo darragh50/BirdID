a1ee0e723ef5f836936af6ee7641c8c253c4b347
darragh50 <darraghrohan@gmail.com>
2025-11-04 18:20:22 +0000

    Add databases.py to manage SQLAlchemy and PostgreSQL connection

---
+++ backend/databases.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Database connection URL for PostgreSQL
+# Format : postgresql://<username>:<password>@<host>:<port>/<database_name>
+DATABASE_URL = "postgresql://postgres:admin@localhost:5432/bird_identifer_db"
+
+# Create databse engine
+engine = create_engine(DATABASE_URL)
+
+# Create a session factory 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for declarative models
+Base = declarative_base()
+
+# Dependency to get a DB session
+def get_db():
+    """
+    Generator function that provides a database session
+    Ensures it is closed after use
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
