d076d9306a0cbec8ab1b15a91d82ed4657856dea
darragh50 <darraghrohan@gmail.com>
2025-11-14 19:28:00 +0000

    Test firebase admin SDK connection

---
+++ backend/testfirebase.py
@@ -0,0 +1,19 @@
+from firebaseconfig import test_firebase_connection, get_user_by_uid
+import firebase_admin
+
+
+# Test firebase connection
+def main():
+    """
+    Test firebase admin SDK
+    """
+    print("Testing Firebase connection")
+    if test_firebase_connection():
+        print("Firebase connection successful")
+    else:
+        print("Firebase connection failed")
+        return
+ 
+# Ensures this code only runs when you execute this file directly
+if __name__ == "__main__":
+    main()
